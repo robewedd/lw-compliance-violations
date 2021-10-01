b9147642c9f4a6a3c189a9dd30ba5900c76caf9a
robewedd <85222250+robewedd@users.noreply.github.com>
2021-10-01 12:09:34 +0200

    Update get-resource.sh

--- get-resource.sh
+++ get-resource.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-curl -X GET -H 'Content-Type: application/json' -H 'Authorization: Bearer '$(lacework access-token) --header 'Account-name: 'lw-tsm  https://lwcs.fra.lacework.net/api/v1/external/compliance/aws/GetLatestComplianceReport\?AWS_ACCOUNT_ID\=680354150194\&FILE_FORMAT\=json |  jq '.results[] | {account_id?}'
+curl -X GET -H 'Content-Type: application/json' -H 'Authorization: Bearer '$(lacework access-token) --header 'Account-name: '${lacework_subaccount}  https://${lacework_tenant}.fra.lacework.net/api/v1/external/compliance/aws/GetLatestComplianceReport\?AWS_ACCOUNT_ID\=680354150194\&FILE_FORMAT\=json |  jq '.results[] | {account_id?}'
